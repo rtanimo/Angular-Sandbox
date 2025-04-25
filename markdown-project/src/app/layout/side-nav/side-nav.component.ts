eab37022fd0ed8170cce183494b26e393686e734
Roi Tanimoto <roi.tanimoto@gmail.com>
2025-04-25 01:31:53 -1000

    boostrap offcanvas

--- markdown-project/src/app/layout/side-nav/side-nav.component.ts
+++ markdown-project/src/app/layout/side-nav/side-nav.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal, TemplateRef, WritableSignal } from '@angular/core';
+import {
+  NgbOffcanvas,
+  OffcanvasDismissReasons,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-side-nav',
   imports: [],
   templateUrl: './side-nav.component.html',
-  styleUrl: './side-nav.component.scss'
+  styleUrl: './side-nav.component.scss',
 })
 export class SideNavComponent {
+  closeResult: WritableSignal<string> = signal('');
 
+  constructor(private offcanvasService: NgbOffcanvas) {}
+
+  open(content: TemplateRef<any>) {
+    this.offcanvasService
+      .open(content, { ariaLabelledBy: 'offcanvas-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult.set(`Closed with: ${result}`);
+        },
+        (reason) => {
+          this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    switch (reason) {
+      case OffcanvasDismissReasons.ESC:
+        return 'by pressing ESC';
+      case OffcanvasDismissReasons.BACKDROP_CLICK:
+        return 'by clicking on the backdrop';
+      default:
+        return `with: ${reason}`;
+    }
+  }
 }
